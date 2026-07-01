d8f71014a6f1a7cf35bbc12bf6e49be15cc53fa9
fvmaia <upciga.app@gmail.com>
2026-07-01 00:15:37 -0300

    feat: adiciona ModuloSubmissaoArtigo com seam de validacao para a cadeia

--- src/main/java/br/edu/ifpb/pps/mediator/MediatorSistema.java
+++ src/main/java/br/edu/ifpb/pps/mediator/MediatorSistema.java
@@ -1,23 +1,32 @@
 package br.edu.ifpb.pps.mediator;
 
+import br.edu.ifpb.pps.model.AreaTematica;
+import br.edu.ifpb.pps.model.Artigo;
+import br.edu.ifpb.pps.model.Pesquisador;
 import br.edu.ifpb.pps.modulos.ModuloCadastroPesquisador;
 import br.edu.ifpb.pps.modulos.ModuloEvento;
+import br.edu.ifpb.pps.modulos.ModuloSubmissaoArtigo;
 import br.edu.ifpb.pps.notificacao.ServicoNotificacao;
+
+import java.util.List;
 
 public class MediatorSistema {
 
     private final ModuloCadastroPesquisador moduloCadastro;
     private final ModuloEvento moduloEvento;
+    private final ModuloSubmissaoArtigo moduloSubmissao;
     private final ServicoNotificacao servicoNotificacao;
 
     public MediatorSistema(ModuloCadastroPesquisador moduloCadastro, ModuloEvento moduloEvento,
-                           ServicoNotificacao servicoNotificacao) {
+                           ModuloSubmissaoArtigo moduloSubmissao, ServicoNotificacao servicoNotificacao) {
         this.moduloCadastro = moduloCadastro;
         this.moduloEvento = moduloEvento;
+        this.moduloSubmissao = moduloSubmissao;
         this.servicoNotificacao = servicoNotificacao;
 
         moduloCadastro.setMediator(this);
         moduloEvento.setMediator(this);
+        moduloSubmissao.setMediator(this);
     }
 
     public void cadastrarAreaTematica(String descricao) {
@@ -26,6 +35,11 @@ public class MediatorSistema {
 
     public void convidarRevisor(String email) {
         moduloCadastro.convidarRevisor(email);
+    }
+
+    public Artigo submeterArtigo(String titulo, Pesquisador autor, String resumo, String nomePDF,
+                                 List<AreaTematica> areasTematicas) {
+        return moduloSubmissao.submeter(titulo, autor, resumo, nomePDF, areasTematicas);
     }
 
     public void iniciarEvento() {
@@ -46,5 +60,9 @@ public class MediatorSistema {
 
     public ModuloEvento getModuloEvento() {
         return moduloEvento;
+    }
+
+    public ModuloSubmissaoArtigo getModuloSubmissao() {
+        return moduloSubmissao;
     }
 }
